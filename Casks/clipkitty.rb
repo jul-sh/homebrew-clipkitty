b0c0d151c8ce11155f38412346b9375e96d3755f
Jul <jul@example.com>
2026-02-05 06:14:08 -0500

    Fix download URL - remove incorrect v prefix

--- Casks/clipkitty.rb
+++ Casks/clipkitty.rb
@@ -2,7 +2,7 @@ cask "clipkitty" do
   version "1.1.1"
   sha256 "a5846f05e5f88ced5c58e25b00093c01ece7fe6a1f6bba5d0c18d4410020459c"
 
-  url "https://github.com/jul-sh/clipkitty/releases/download/v#{version}/ClipKitty.dmg"
+  url "https://github.com/jul-sh/clipkitty/releases/download/#{version}/ClipKitty.dmg"
   name "ClipKitty"
   desc "Clipboard history manager with instant fuzzy search"
   homepage "https://github.com/jul-sh/clipkitty"
